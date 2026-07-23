499bf3127001729ee91e4b3ed668dfef870bd2f3
G4NDH1N <gandhiegc@gmail.com>
2026-07-23 16:43:12 -0700

    Cambios en la URL, ahora del vercel

--- crud_peliculas/script.js
+++ crud_peliculas/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/peliculas";
+const API_URL = "https://api-peliculas-iota.vercel.app/";
 
 const formulario = document.getElementById("formulario");
 const id = document.getElementById("id");
